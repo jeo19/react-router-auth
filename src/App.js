22ef6f4af02c91f1730b2857c241fe8c730f2287
jeo19 <terrakkk@outlook.com>
2020-11-24 18:59:59 +0800

    add:Add the context API for the authentication logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
 import Home from './pages/Home';
 import Admin from './pages/Admin';
+import { AuthContext } from './context/auth';
 function App(props) {
     return (
-        <Router>
-            <div>
-                <ul>
-                    <li>
-                        <Link to="/">Home page</Link>
-                    </li>
-                    <li>
-                        <Link to="/admin">Admin page</Link>
-                    </li>
-                </ul>
-                <Route exact path="/" component={Home} />
-                <Route path="/admin" component={Admin} />
-            </div>
-        </Router>
+        <AuthContext.Provider value={false}>
+            <Router>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/">Home page</Link>
+                        </li>
+                        <li>
+                            <Link to="/admin">Admin page</Link>
+                        </li>
+                    </ul>
+                    <Route exact path="/" component={Home} />
+                    <PrivateRoute path="/admin" component={Admin} />
+                </div>
+            </Router>
+        </AuthContext.Provider>
     );
 }
 
